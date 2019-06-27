aeac79c2ab8983672fddcbeccf6f0aba893e8d76
Arto Bendiken <arto@conreality.org>
2019-06-27 22:45:39 +0300

    Hooked up the Dart-to-Swift-to-Go backend method channel.

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -1,5 +1,8 @@
+/* This is free and unencumbered software released into the public domain. */
+
 import UIKit
 import Flutter
+import Developer
 
 @UIApplicationMain
 @objc class AppDelegate: FlutterAppDelegate {
@@ -7,7 +10,23 @@ import Flutter
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?
   ) -> Bool {
+
+    let controller: FlutterViewController = window?.rootViewController as! FlutterViewController
+    let channel = FlutterMethodChannel(name: "app.conreality/developer", binaryMessenger: controller)
+    channel.setMethodCallHandler({
+      [weak self] (call: FlutterMethodCall, result: FlutterResult) -> Void in
+      if call.method == "getVersion" {
+        self?.receiveVersion(result: result)
+        return
+      }
+      result(FlutterMethodNotImplemented)
+    })
+
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
+  }
+
+  private func receiveVersion(result: FlutterResult) {
+    result(DeveloperVersion())
   }
 }
