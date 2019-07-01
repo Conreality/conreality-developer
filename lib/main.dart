f9e7c49be4b3593c103ce27aa6d40060fe57430a
Arto Bendiken <arto@conreality.org>
2019-07-01 14:47:40 +0300

    Configured flutter_background_geolocation.

--- lib/main.dart
+++ lib/main.dart
@@ -10,9 +10,49 @@ import 'src/gdk.dart' show GDK, GDKThread;
 ////////////////////////////////////////////////////////////////////////////////
 
 void main() async {
+  bg.BackgroundGeolocation.onProviderChange((final bg.ProviderChangeEvent event) {
+    print('BackgroundGeolocation.onProviderChange: $event'); // TODO
+  });
+  bg.BackgroundGeolocation.onMotionChange((final bg.Location location) {
+    print('BackgroundGeolocation.onMotionChange: $location'); // TODO
+  });
+  bg.BackgroundGeolocation.onActivityChange((final bg.ActivityChangeEvent event) {
+    print('BackgroundGeolocation.onActivityChange: $event'); // TODO
+  });
+  bg.BackgroundGeolocation.onLocation((final bg.Location location) {
+    print('BackgroundGeolocation.onLocation: $location'); // TODO
+  });
+  bg.BackgroundGeolocation.onHeartbeat((final bg.HeartbeatEvent event) {
+    print('BackgroundGeolocation.onHeartbeat: $event'); // TODO
+  });
+  bg.BackgroundGeolocation.ready(bg.Config(
+    reset: true,
+    startOnBoot: false,
+    stopOnTerminate: true, // don't continue tracking after app terminates
+    //enableHeadless: true, // FIXME
+    foregroundService: true,
+    notification: bg.Notification(
+      channelName: "Conreality Developer",
+      title: "Conreality Developer",
+      text: "Tracking your location.",
+      priority: bg.Config.NOTIFICATION_PRIORITY_HIGH,
+    ),
+    heartbeatInterval: 60, // the minimum interval on Android is 60s
+    desiredAccuracy: bg.Config.DESIRED_ACCURACY_HIGH,
+    distanceFilter: 3.0, // meters
+    //debug: true,
+    logLevel: bg.Config.LOG_LEVEL_VERBOSE,
+  )).then((final bg.State state) {
+    print('BackgroundGeolocation.ready: $state');
+    if (!state.enabled) {
+      bg.BackgroundGeolocation.start();
+    }
+  });
+
   print("Conreality GDK ${await GDK.version}");
   final thread = await GDKThread.spawn();
   print(await thread.evalAsset("scripts/hello.luac")); // DEBUG
+
   runApp(App());
 }
 
