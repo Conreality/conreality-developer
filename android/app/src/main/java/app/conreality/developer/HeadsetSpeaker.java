dfde773563248bbbceb5bae800f14228f3b399dc
Arto Bendiken <arto@conreality.org>
2019-07-12 18:15:08 +0300

    Hooked up the GDK headset to the conreality_headset plugin.

--- android/app/src/main/java/app/conreality/developer/HeadsetSpeaker.java
+++ android/app/src/main/java/app/conreality/developer/HeadsetSpeaker.java
@@ -2,40 +2,49 @@
 
 package app.conreality.developer;
 
+import android.content.ComponentName;
 import android.content.Context;
-import android.os.Bundle;
-import android.speech.tts.TextToSpeech;
-import java.util.UUID;
+import android.content.Intent;
+import android.util.Log;
 
 /** HeadsetSpeaker */
 final class HeadsetSpeaker implements Headset {
-  private final TextToSpeech.OnInitListener onInitListener =
-    new TextToSpeech.OnInitListener() {
-      @Override
-      public void onInit(final int status) {
-        if (status != TextToSpeech.SUCCESS) {
-          // TODO: error handling
-        }
-      }
-    };
-  private final TextToSpeech tts;
-  private final Bundle params;
-  private UUID utteranceID;
+  private static final String TAG = "ConrealityDeveloper";
+  private static final String SERVICE_CLASS = "app.conreality.plugins.headset.HeadsetService";
+
+  private final Context context;
+  private final Intent intent;
 
   public HeadsetSpeaker(final Context context) {
-    this.tts = new TextToSpeech(context, onInitListener, "com.google.android.tts");
-    this.params = new Bundle();
+    if (Log.isLoggable(TAG, Log.DEBUG)) {
+      Log.d(TAG, String.format("Resolving the headset service class %s...", SERVICE_CLASS));
+    }
+
+    Class serviceClass;
+    try {
+      serviceClass = Class.forName(SERVICE_CLASS);
+    }
+    catch (final ClassNotFoundException error) {
+      throw new RuntimeException(error);
+    }
+
+    this.context = context;
+    this.intent = new Intent(context, serviceClass);
   }
 
   @Override
   public void speak(final String message) {
-    this.utteranceID = UUID.randomUUID();
-    System.out.printf(">>> %s [%s]", message, this.utteranceID.toString()); // TODO
-
-    // See: https://developer.android.com/reference/android/speech/tts/TextToSpeech.html#speak(java.lang.CharSequence,%2520int,%2520android.os.Bundle,%2520java.lang.String)
-    final int result = this.tts.speak(message, TextToSpeech.QUEUE_FLUSH, this.params, this.utteranceID.toString());
-    if (result != TextToSpeech.SUCCESS) {
-      // TODO: error handling
+    if (Log.isLoggable(TAG, Log.INFO)) {
+      Log.i(TAG, String.format("Speaking into the headset: \"%s\"", message));
     }
+
+    final Intent intent = this.makeIntent("speak").putExtra("message", message);
+    if (this.context.startService(intent) == null) {
+      Log.e(TAG, "Failed to invoke the headset service.");
+    }
+  }
+
+  private Intent makeIntent(final String action) {
+    return this.intent.cloneFilter().setAction("speak");
   }
 }
