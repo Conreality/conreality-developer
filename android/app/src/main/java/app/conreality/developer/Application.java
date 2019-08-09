3cbc9ff3514cab64cac03a1716e6c3b2a17922be
Arto Bendiken <arto@conreality.org>
2019-08-09 18:57:48 +0300

    Implemented SDK initialization.

--- android/app/src/main/java/app/conreality/developer/Application.java
+++ android/app/src/main/java/app/conreality/developer/Application.java
@@ -2,23 +2,29 @@
 
 package app.conreality.developer;
 
+import androidx.annotation.CallSuper;
+import com.transistorsoft.flutter.backgroundfetch.BackgroundFetchPlugin;
+import com.transistorsoft.flutter.backgroundgeolocation.FLTBackgroundGeolocationPlugin;
 import io.flutter.app.FlutterApplication;
 import io.flutter.plugin.common.PluginRegistry;
 import io.flutter.plugin.common.PluginRegistry.PluginRegistrantCallback;
 import io.flutter.plugins.GeneratedPluginRegistrant;
+import org.conreality.sdk.android.ConrealitySDK;
 
-import com.transistorsoft.flutter.backgroundfetch.BackgroundFetchPlugin;
-import com.transistorsoft.flutter.backgroundgeolocation.FLTBackgroundGeolocationPlugin;
-
+/** Application */
 public final class Application extends FlutterApplication implements PluginRegistrantCallback {
+  /** Implements android.app.Application#onCreate(). */
+  @CallSuper
   @Override
   public void onCreate() {
     super.onCreate();
+    ConrealitySDK.init(this);
     ApplicationPlugin.setPluginRegistrant(this);
     BackgroundFetchPlugin.setPluginRegistrant(this);
     FLTBackgroundGeolocationPlugin.setPluginRegistrant(this);
   }
 
+  /** Implements io.flutter.plugin.common.PluginRegistry.PluginRegistrantCallback#registerWith(). */
   @Override
   public void registerWith(final PluginRegistry registry) {
     GeneratedPluginRegistrant.registerWith(registry);
